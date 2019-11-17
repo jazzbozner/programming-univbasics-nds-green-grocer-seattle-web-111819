e01484bac2941b8e4025322b5de2759239eb47be
Jazz Bozner <jazzbozner@gmail.com>
2019-11-17 00:43:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,13 +21,13 @@ def consolidate_cart(cart)
   final_array = []
   cart.each do |element_hash|
     hash_index = element_hash[0]
-    
+     binding.pry
     
     if final_array[element_hash]
     else
     
       
-  binding.pry
+ 
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
