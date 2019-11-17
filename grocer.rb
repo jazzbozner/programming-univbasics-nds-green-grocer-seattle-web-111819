867605bf986ef1c80695cea8cc0217ae4246ecd2
Jazz Bozner <jazzbozner@gmail.com>
2019-11-17 00:49:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,8 +20,7 @@ end
 def consolidate_cart(cart)
   final_array = []
   cart.each do |element_hash|
-    element_item = element_hash[:item]
-    element_price = element_has[:price]
+    element_item = .keys[0]
      binding.pry
     
     # if final_array[element_hash]
