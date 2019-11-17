d02db9e557f5d048d854034113bc560b0ae8e1b5
Jazz Bozner <jazzbozner@gmail.com>
2019-11-17 02:24:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,24 @@ end
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
 def apply_coupons(cart, coupons)
+    coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+    
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
   
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
+end
   
 #   coupons.each do |coupon|
 #     item = coupon[:item]
