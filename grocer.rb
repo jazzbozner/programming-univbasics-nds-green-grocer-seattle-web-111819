83bd349af74ad00f8fe593d7591e33da12c406b9
Jazz Bozner <jazzbozner@gmail.com>
2019-11-17 02:25:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -104,10 +104,17 @@ end
 
 
 def apply_clearance(cart)
+  cart.each do |item, attribute_hash| 
+    if attribute_hash[:clearance] == true 
+      attribute_hash[:price] = (attribute_hash[:price] *
+      0.8).round(2) 
+    end 
+  end 
+cart 
+end
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
