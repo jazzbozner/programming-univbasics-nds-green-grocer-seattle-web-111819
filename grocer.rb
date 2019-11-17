e91346e58c60161fabaaf9729933569258d2f95a
Jazz Bozner <jazzbozner@gmail.com>
2019-11-17 01:50:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,22 +45,22 @@ end
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-# def apply_coupons(cart, coupons)
-#   i = 0
-#   while i < coupons.count do
-#     coupon = coupons[i]
-#     item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
-#     item_is_in_basket = !!item_with_coupon
-#     count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
+def apply_coupons(cart, coupons)
+  i = 0
+  while i < coupons.count do
+    coupon = coupons[i]
+    item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
+    item_is_in_basket = !!item_with_coupon
+    count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
 
-#     if item_is_in_basket and count_is_big_enough_to_apply
-#       apply_coupon_to_cart(item_with_coupon, coupon, cart)
-#   binding.pry
-#     end
-#     i += 1
-#   end
-#   cart
-# end
+    if item_is_in_basket and count_is_big_enough_to_apply
+      apply_coupon_to_cart(item_with_coupon, coupon, cart)
+  binding.pry
+    end
+    i += 1
+  end
+  cart
+end
 
   # Consult README for inputs and outputs
   #
