9bed93b191b6b58fa5b23dd2f1b8728af82fb14d
Jazz Bozner <jazzbozner@gmail.com>
2019-11-17 01:02:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -61,7 +61,6 @@ end
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  end
 end
 
 def apply_coupons(cart, coupons)
